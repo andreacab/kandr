5b3f556ea1efefcd87bcf48dd9057247cdc9a223
Andrea Cabral <andrea.cabralb@gmail.com>
2017-06-16 06:07:00 -0700

    adds ex 2_4

--- ch2/2_4.c
+++ ch2/2_4.c
@@ -7,8 +7,8 @@ void squeeze(char[], char[]);
 int main(int argc, char* argv[]) {
   int i;
   char str[4] = "abc";
-  
-  char* test[5] = {
+
+  char test[5][4] = {
     "aaa",
     "abb",
     "cba",
@@ -29,21 +29,21 @@ int main(int argc, char* argv[]) {
 
 void squeeze(char s1[], char s2[]) {
   int add, m, i, j;
-  printf("s1: %s\n", s1);
-  printf("s2: %s\n", s2);
+
   m = 0;
-//  for (i = 0; s1[i] != EOS; i++) {
-//    add = 1;
-//    
-//    for (j = 0; s2[j] != EOS; j++) {
-//      if (s1[i] == s2[j]) {
-//        add = 0;
-//        break;
-//      }
-//    }
-//    if (add)
-//      s1[m++] = s1[i];
-//  }
-  printf("m: %d\n", m);
+  for (i = 0; s1[i] != EOS; i++) {
+    add = 1;
+
+    for (j = 0; s2[j] != EOS; j++) {
+      if (s1[i] == s2[j]) {
+        add = 0;
+        break;
+      }
+    }
+    if (add) {
+      s1[m] = s1[i];
+      m++;
+    }
+  }
   s1[m] = EOS;
 }
